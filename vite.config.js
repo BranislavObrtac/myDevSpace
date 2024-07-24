7f07eb1862027373e555d2f8d2b4e6aa320be8c1
Branislav <73939297+BranislavObrtac@users.noreply.github.com>
2024-07-24 09:25:26 +0200

    chore: Add vite-plugin-svgr for SVG support and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-import viteReact from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), TanStackRouterVite()],
+  plugins: [viteReact(), TanStackRouterVite(), svgr()],
+  css: {
+    modules: {
+      generateScopedName: isProduction
+        ? "[hash:base64:5]"
+        : "[name]__[local]___[hash:base64:5]",
+    },
+  },
+  build: {
+    sourcemap: false,
+  },
 });
