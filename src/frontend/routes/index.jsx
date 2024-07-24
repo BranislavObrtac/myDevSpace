ccf57b44919844f64a2abbcefdec4423860e318a
Branislav <73939297+BranislavObrtac@users.noreply.github.com>
2024-07-24 11:10:18 +0200

    feat: Add contact, experience, and work routes
    Added new routes for contact, experience, and work pages using the Tanstack Router library. Each route has a corresponding component that renders a simple greeting message. This update enhances the website's navigation and provides additional content for users to explore.

--- src/frontend/routes/index.jsx
+++ src/frontend/routes/index.jsx
@@ -1,18 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Home from "../components/home/Home";
 
 export const Route = createFileRoute("/")({
-  component: () => (
-    <div>
-      <div>Hello /!</div>
-      <p>
-        I am a software engineer with a passion for building high-quality
-        software. I have experience in a wide range of technologies and
-        languages, including JavaScript, TypeScript, Python, and Java. I have
-        worked on a variety of projects, from small web applications to large
-        enterprise systems. I am always looking to learn new things and improve
-        my skills. In my free time, I enjoy reading, hiking, and playing video
-        games.
-      </p>
-    </div>
-  ),
+  component: () => <Home />,
 });
