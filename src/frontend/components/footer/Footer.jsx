009c56a200184d30609d8912318eb3d6bdfd2e1f
Branislav <73939297+BranislavObrtac@users.noreply.github.com>
2024-08-26 12:01:17 +0200

    chore: Add Socials component to Footer

--- src/frontend/components/footer/Footer.jsx
+++ src/frontend/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import Socials from "../socials/Socials";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.inner}>
         ©{new Date().getFullYear()} MyDevSpace Branislav Obrtáč
+        <Socials />
       </div>
     </div>
   );
